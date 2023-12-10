fae8d35741223b573e37ff960ed3dc979c2796fd
JonGuy-Ima <jguy11230@gmail.com>
2023-12-10 02:31:15 -0500

    Day2 Done

--- day2/day2.py
+++ day2/day2.py
@@ -1,11 +1,33 @@
-with open("input.txt", "r") as lines:
-    maxR = 12
-    maxG = 13
-    maxB = 14
+file = open("input.txt", "r")
+lines = file.readlines()
+file.close()
 
-    for line in lines:
-        id = int(line.split(":")[0].split(" ")[1])
-        colors = line.split(":")[1].split(";")
+sum = 0
+maxR = 12
+maxG = 13
+maxB = 14
+
+for line in lines:
+    flag = True
+    id = int(line.split(":")[0].split(" ")[1])
+    rounds = line.split(":")[1].split(";")
+
+    for round in rounds:
+        colors = round.split(",")
 
         for color in colors:
-            print(color)
+            choice = color.strip().split(" ")
+            match choice[1]:
+                case "red":
+                    if int(choice[0]) > maxR:
+                        flag = False
+                case "green":
+                    if int(choice[0]) > maxG:
+                        flag = False
+                case "blue":
+                    if int(choice[0]) > maxB:
+                        flag = False
+    if flag:
+        sum += id
+
+print(sum)
